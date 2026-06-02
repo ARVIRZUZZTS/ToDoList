6ecf46731984d29e0873ccd6106f7ea5b03d2292
Jaime Sebastian Chavarria Fuertes <jschavarria77@gmail.com>
2026-06-02 16:48:33 -0400

    feat: added er user model

---
+++ pgmodeler/scriptuser.sql
@@ -0,0 +1,78 @@
+-- ** Database generated with pgModeler (PostgreSQL Database Modeler).
+-- ** pgModeler version: 1.2.3
+-- ** PostgreSQL version: 17.0
+-- ** Project Site: pgmodeler.io
+-- ** Model Author: ---
+
+-- ** Database creation must be performed outside a multi lined SQL file. 
+-- ** These commands were put in this file only as a convenience.
+
+-- object: "TaskFile" | type: DATABASE --
+-- DROP DATABASE IF EXISTS "TaskFile";
+CREATE DATABASE "TaskFile";
+-- ddl-end --
+
+
+SET search_path TO pg_catalog,public;
+-- ddl-end --
+
+-- object: public."Task" | type: TABLE --
+-- DROP TABLE IF EXISTS public."Task" CASCADE;
+CREATE TABLE public."Task" (
+	task_id varchar(128) NOT NULL,
+	name varchar(64),
+	description text,
+	completed bool,
+	"createdAt" timestamp,
+	priority smallint,
+	id_usuario varchar(40),
+	CONSTRAINT "Task_pk" PRIMARY KEY (task_id)
+);
+-- ddl-end --
+ALTER TABLE public."Task" OWNER TO postgres;
+-- ddl-end --
+
+-- object: public."File" | type: TABLE --
+-- DROP TABLE IF EXISTS public."File" CASCADE;
+CREATE TABLE public."File" (
+	id_file serial NOT NULL,
+	name varchar(48),
+	path text,
+	uploaded_at timestamp,
+	last_download_at timestamp,
+	task_id varchar(128),
+	CONSTRAINT "FIle_pk" PRIMARY KEY (id_file)
+);
+-- ddl-end --
+ALTER TABLE public."File" OWNER TO postgres;
+-- ddl-end --
+
+-- object: public."User" | type: TABLE --
+-- DROP TABLE IF EXISTS public."User" CASCADE;
+CREATE TABLE public."User" (
+	id_usuario varchar(40) NOT NULL,
+	name varchar(256),
+	email varchar(256),
+	password_hash varchar(256),
+	"createdAt" timestamptz,
+	CONSTRAINT "User_pk" PRIMARY KEY (id_usuario)
+);
+-- ddl-end --
+ALTER TABLE public."User" OWNER TO postgres;
+-- ddl-end --
+
+-- object: "fk_id_usuario(User-Task)" | type: CONSTRAINT --
+-- ALTER TABLE public."Task" DROP CONSTRAINT IF EXISTS "fk_id_usuario(User-Task)" CASCADE;
+ALTER TABLE public."Task" ADD CONSTRAINT "fk_id_usuario(User-Task)" FOREIGN KEY (id_usuario)
+REFERENCES public."User" (id_usuario) MATCH SIMPLE
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- ddl-end --
+
+-- object: "fk-task_id(File)-task_id(Task)" | type: CONSTRAINT --
+-- ALTER TABLE public."File" DROP CONSTRAINT IF EXISTS "fk-task_id(File)-task_id(Task)" CASCADE;
+ALTER TABLE public."File" ADD CONSTRAINT "fk-task_id(File)-task_id(Task)" FOREIGN KEY (task_id)
+REFERENCES public."Task" (task_id) MATCH SIMPLE
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- ddl-end --
+
+

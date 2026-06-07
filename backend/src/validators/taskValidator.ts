e9551282f137b4bed9b0d3ebc985a0ceea06d374
Jaime Sebastian Chavarria Fuertes <jschavarria77@gmail.com>
2026-06-06 23:51:27 -0400

    feat: added zod schema for validate tasks

--- backend/src/validators/taskValidator.ts
+++ backend/src/validators/taskValidator.ts
@@ -1,8 +1,31 @@
-import {z} from 'zod';
+import { getPriority } from "node:os";
+import { z } from "zod";
 
-export const statusSchema = z.object({
-  completed:z.boolean({
-    error: "El campo 'completed' debe ser booleano"
+const nameField = z
+  .string()
+  .transform((s) => s.trim())
+  .pipe(
+    z
+      .string()
+      .min(1, "El nombre es obligatorio")
+      .max(64, "El nombre es demasiado largo, maximo 64 caracteres"),
+  );
+
+export const createTaskSchema = z.object({
+  name: nameField,
+  description: z
+    .string()
+    .transform((s) => s.trim())
+    .pipe(z.string().max(1000, "La descripcion es demasiado larga"))
+    .optional(),
+  priority: z.number({error:"La prioridad debe de ser un numero"}).int("La prioridad debe ser un numero entero").min(0, "La prioridad no puede ser negativa").max(10, "La prioridad maxima es 10")
+});
+
+export const updateTaskStatusSchema = z.object({
+  completed: z.boolean({
+    error: "El campo 'completed' debe de ser booleano"
   })
 });
 
+export type CreateTaskInput = z.infer<typeof createTaskSchema>;
+export type UpdateTaskStatusInput = z.infer<typeof updateTaskStatusSchema>;

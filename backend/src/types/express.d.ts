fa18703157d638b8f505b6adde89699aa7999476
Jaime Sebastian Chavarria Fuertes <jschavarria77@gmail.com>
2026-06-07 15:19:19 -0400

    feat: added file to request type for express

--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -1,9 +1,11 @@
+import 'multer';
 declare global{
   namespace Express{
     interface Request{
       user?:{
         user_id:string
-      }
+      };
+      file?:Express.Multer.File;
     }
   }
 }
